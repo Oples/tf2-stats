ac8ad9015364247a8d37e2b2529f63606b36956a
laykeen <lynxnic99@gmail.com>
2021-03-29 18:10:49 +0200

    Add filterPlayersList javascript on map.html

--- web/fun.js
+++ web/fun.js
@@ -4,6 +4,15 @@ const boxType = {
   CHAT: "chat",
 }
 
+// Filters links to player's data array index
+const filterType = {
+    NAME: 0,
+    TEAM: 1,
+    KDRATIO: 2,
+    CRIT: 3,
+    WEAPON: 4
+}
+
 function toggleShowBox(elemId, button_type) {
   var list1Elem = null;
   var list2Elem = null;
@@ -35,4 +44,54 @@ function toggleShowBox(elemId, button_type) {
     list1Elem.classList.add('transition-h4');
     list2Elem.classList.add('h0');
   }
+}
+
+
+function filterPlayersList(input, filterType) {
+    let allPlayersData = [];
+    // get every player's container
+    let playersList = document
+        .getElementById("players_list")
+        .getElementsByTagName("li");
+
+    // for every player
+    for (let i = 0; i < playersList.length; i++) {
+        let playerData = [];
+        // extract player's data to filter it after
+        let playerField = playersList[i].getElementsByClassName("player_field");
+        for (let x = 0; x < playerField.length; x++) {
+            let singleVal = playerField[x].getElementsByTagName("label");
+            // accept only readable names to filter
+            if(singleVal.length > 0) {
+                singleVal = singleVal[0].innerText;
+            }
+            else {
+                singleVal = null;
+            }
+            playerData.push(singleVal);
+        }
+        allPlayersData.push({
+            container: playersList[i],
+            data: playerData
+        });
+    }
+    // filter everything
+    for (var i = 0; i < allPlayersData.length; i++) {
+        if(allPlayersData[i].data[filterType] != null) {
+            let inputUp = input.toUpperCase();
+            let dataUp = allPlayersData[i].data[filterType].toUpperCase();
+            // show names if filter matches or there is no filter
+            if(dataUp.indexOf(inputUp) !== -1 || inputUp.length <= 0) {
+                allPlayersData[i].container.classList.remove(
+                    'h0', 'dn'
+                );
+            }
+            // else hide this player's container
+            else {
+                allPlayersData[i].container.classList.add(
+                    'h0', 'dn'
+                );
+            }
+        }
+    }
 }
