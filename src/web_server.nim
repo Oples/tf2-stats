55712ac2a0ae7533228238b1d0f95d71928e9340
Oples <oples.treasure@gmail.com>
2022-03-21 01:43:45 +0100

    re-work in progress for the WebSocket events

--- src/web_server.nim
+++ src/web_server.nim
@@ -1,15 +1,15 @@
 #                                                  #
 # Under MIT License                                #
-# Author: (c) 2021 Oples                           #
+# Author: (c) 2022 Oples                           #
 # Original repo can be found at:                   #
 #      https://github.com/Oples/tf2-stats          #
 #                                                  #
-import std/[asynchttpserver, asyncdispatch, strutils]
+import std/[asynchttpserver, asyncdispatch, strutils, asyncfutures, asyncstreams]
 import std/[os, json, logging, times, mimetypes]
 import ws
 import tail_console
 import obj/[Game, Kill, Chat]
-import std/browsers
+import std/strformat
 
 ##[
   Guide:
@@ -18,17 +18,39 @@ import std/browsers
       - /template is templates (loaded with @{include ""})
 ]##
 var logger {.threadvar.} : ConsoleLogger
-var chan : Channel[string]
-var
-    chanTF2Matches : Channel[JsonNode]
-    chanTF2Kills : Channel[JsonNode]
-    chanTF2Chat : Channel[JsonNode]
+#var chan : Channel[string]
+#var
+#    chanTF2Matches : Channel[JsonNode]
+#    chanTF2Kills : Channel[JsonNode]
+#    chanTF2Chat : Channel[JsonNode]
+# https://nim-lang.org/docs/asyncstreams.html#newFutureStream%2Cstring
+var chatAsync : JsonNode
 var WebPort = 9844
 var FilePath* = ""
 const WebDir = "web"
 
 logger = newConsoleLogger(levelThreshold=lvlDebug, fmtStr="[$time] - $levelname: ")
 
+
+proc createTestJson(urlWS: string): string =
+    result = """<html>
+    <body>
+        <pre id="consoleOut" style="font-family:monospace;white-space: pre;"></pre>
+        <script>
+        let socket = new WebSocket("ws://" + window.location.host + "/{urlWS}");
+        var out = document.getElementById("consoleOut");
+
+        socket.onmessage = function (evt) {
+            //console.log('RESPONSE: ' + evt.data);
+            try {
+                out.innerHTML = JSON.stringify(JSON.parse(evt.data), null, 4) + out.innerHTML;
+                console.log(JSON.parse(evt.data));
+            } catch {
+            }
+        }
+        </script>
+        </body>
+    </html>""".replace("{urlWS}", urlWS)
 
 proc createTestHtml(urlWS: string): string =
     result = """<html>
@@ -50,50 +72,57 @@ proc createTestHtml(urlWS: string): string =
         </body>
     </html>""".replace("{urlWS}", urlWS)
 
-
+proc toLog(data: JsonNode, logType: string): JsonNode =
+    result = newJObject()
+    result.add("type", newJString(logType))
+    result.add("data", data)
 
 proc tf2logger() {.thread.} =
-    logger = newConsoleLogger(levelThreshold=lvlDebug, fmtStr="[$time] - $levelname: ")
-    var consoleLogger = newTF2ConsoleLogger()
+    var asyncLogger = proc () {.async.} =
+        logger = newConsoleLogger(levelThreshold=lvlDebug, fmtStr="[$time] - $levelname: ")
+        var consoleLogger = newTF2ConsoleLogger()
+        var wsRaw = await newWebSocket(fmt"ws://127.0.0.1:{WebPort}/setRawWS")
+        var wsUpdate = await newWebSocket(fmt"ws://127.0.0.1:{WebPort}/newUpdateWS")
 
-    consoleLogger.onNewLine = proc(self: TF2ConsoleLogger, line: string) =
-        chan.send line
+        consoleLogger.onNewLine = proc(self: TF2ConsoleLogger, line: string) =
+            when isMainModule:
+                sleep(300)
+            waitFor wsRaw.send($line)
 
-    consoleLogger.afterNewLine = proc(self: TF2ConsoleLogger, line: string) =
-        chanTF2Matches.send self.game.toJson
+        consoleLogger.afterNewLine = proc(self: TF2ConsoleLogger, line: string) =
+            #chanTF2Matches.send self.game.toJson
+            discard
 
-    consoleLogger.onKill = proc(self: TF2ConsoleLogger, k: Kill) =
-        chanTF2Kills.send k.toJson
+        consoleLogger.onKill = proc(self: TF2ConsoleLogger, k: Kill) =
+            waitFor wsUpdate.send $toLog(k.toJson, "kill")
+            discard
 
-    consoleLogger.onChatMessage = proc(self: TF2ConsoleLogger, c: Chat) =
-        chanTF2Chat.send c.toJson
+        consoleLogger.onChatMessage = proc(self: TF2ConsoleLogger, c: Chat) =
+            waitFor wsUpdate.send $toLog(c.toJson, "chat")
 
-    {.gcsafe.}:
-        consoleLogger.runWatchdog(FilePath)
+        when isMainModule:
+            consoleLogger.runWatchdog("test/console.log")
+        else:
+            consoleLogger.runWatchdog(FilePath)
 
+    waitFor asyncLogger()
 
 proc testWS() {.thread.} =
-    logger = newConsoleLogger(levelThreshold=lvlDebug, fmtStr="[$time] - $levelname: ")
-    var consoleLogger = newTF2ConsoleLogger()
+    var asyncLoggerTest = proc () {.async.} =
+        logger = newConsoleLogger(levelThreshold=lvlDebug, fmtStr="[$time] - $levelname: ")
+        var consoleLogger = newTF2ConsoleLogger()
+        var wsRaw = await newWebSocket(fmt"ws://127.0.0.1:{WebPort}/setRawWS")
 
-    consoleLogger.onNewLine = proc(self: TF2ConsoleLogger, line: string) =
-        chan.send line
+        consoleLogger.onNewLine = proc(self: TF2ConsoleLogger, line: string) =
+            waitFor wsRaw.send($line)
 
-    consoleLogger.afterNewLine = proc(self: TF2ConsoleLogger, line: string) =
-        chanTF2Matches.send self.game.toJson
-        logger.log(lvlInfo, line)
+        consoleLogger.afterNewLine = proc(self: TF2ConsoleLogger, line: string) =
+            #logger.log(lvlInfo, line)
+            discard
 
-    consoleLogger.runWatchdog("test/console.log")
+        consoleLogger.runWatchdog("test/console.log")
 
-    #[while true:
-        echo "TF waiting"
-        sleep(4 * 1000) # seconds
-        echo "TF done"
-        echo "id: ", i
-        i += 1
-        dispatcher.add("boop" & $i, newJBool(true))
-        chan.send dispatcher
-        echo "TF signal!"]#
+    waitFor asyncLoggerTest()
 
 
 proc getSelfWebPageContent(filePagePath: string): string =
@@ -104,131 +133,81 @@ proc getSelfWebPageContent(filePagePath: string): string =
 proc runHTTPServer() {.thread.} =
     ## Start listener server for incoming HTTP requests
     var server = newAsyncHttpServer()
-    var matchesLog = newJObject()
-    var killsLog : seq[JsonNode]
-    var chatLog : seq[JsonNode]
     var lineRawLog : seq[string]
+    var logHistory = newJObject()
+    var futureRawLine = newFutureStream[string]("setRaw")
+    var futureUpdate = newFutureStream[string]("newUpdate")
+    var conClients : seq[WebSocket]
 
-    proc cb(req: Request) {.async.} =
+    logHistory.add("old", newJArray())
+
+    proc cb(req: Request) {.async, gcsafe.} =
         var mime = newMimetypes()
 
         let headerTime = now()
         var headers = {"Date": headerTime.format("ddd, dd MMM YYYY HH:mm:ss ") & headerTime.timezone.name,
                        "Content-type": "text/html; charset=utf-8"}
 
-        var dataToSend = "TF2 Logger! If you are seeing this I genuinely don't know how you did that"
-
+        var dataToSend = "TF2 Logger! If you are seeing this I genuinely don't know how this happened"
 
         if req.url.path == "/raw":
             dataToSend = createTestHtml("rawWS")
             await req.respond(Http200, dataToSend, headers.newHttpHeaders())
 
+        if req.url.path == "/update":
+            dataToSend = createTestJson("updateWS")
+            await req.respond(Http200, dataToSend, headers.newHttpHeaders())
 
         if req.url.path == "/testMatches":
             dataToSend = createTestHtml("getMatchesWS")
             await req.respond(Http200, dataToSend, headers.newHttpHeaders())
 
-
         elif req.url.path == "/rawWS":
             var ws = await newWebSocket(req)
             await ws.send(":: Welcome to Team Fortress 2 Logger")
             await ws.send("")
-            var lineRawLogIndex = 0
+            if ws.readyState == Open:
+                dataToSend = ""
+                for logLine in lineRawLog:
+                    dataToSend = logLine & "\n" & dataToSend
+                await ws.send(dataToSend)
             while ws.readyState == Open:
-                if lineRawLog.len > lineRawLogIndex:
-                    dataToSend = ""
-                    for i in lineRawLogIndex..<lineRawLog.len:
-                        dataToSend = lineRawLog[i] & "\n" & dataToSend
-                        lineRawLogIndex += 1
-                    await ws.send(dataToSend)
-                else: await sleepAsync(20)
+                var (newData, dataToSend) = await futureRawLine.read()
+                await ws.send(dataToSend)
+                #await sleepAsync(2000)
 
-
-        elif req.url.path == "/getMatchesWS":
+        elif req.url.path == "/setRawWS":
             var ws = await newWebSocket(req)
-            var noMatches = 0
             while ws.readyState == Open:
-                if matchesLog["matches"].len > noMatches:
-                    await ws.send(matchesLog.pretty)
-                    noMatches += 1
-                else: await sleepAsync(20)
+                let dataRecv = await ws.receiveStrPacket()
+                #echo ":: dataRecv ",dataRecv
+                lineRawLog.add dataRecv
+                await futureRawLine.write(dataRecv & "\n")
 
-
-        elif req.url.path == "/hookMatchWS":
+        elif req.url.path == "/updateWS":
             var ws = await newWebSocket(req)
-            var logsRead = 0
+            conClients.add ws
+            if ws.readyState == Open:
+                dataToSend = $logHistory
+                await ws.send(dataToSend)
             while ws.readyState == Open:
-                if matchesLog.hasKey("matches"):
-                    if logsRead > matchesLog["matches"].len: logsRead = 0
-                    let numLogsToRead = matchesLog["matches"].len
-                    if numLogsToRead > logsRead:
-                        var node = newJObject()
-                        var msg = newJArray()
-                        for i in logsRead..<numLogsToRead:
-                            msg.add(matchesLog["matches"][i])
-                        node.add("update", msg)
-                        await ws.send($node)
-                        logsRead = numLogsToRead
-                    else: await sleepAsync(20)
+                var (dataFlag, dataToSend) = await futureUpdate.read()
+                if dataFlag:
+                    var newData = newJObject()
+                    newData.add("new", parseJson(dataToSend))
+                    for cli in conClients:
+                        await cli.send($newData)
+                    #await sleepAsync(2000)
 
-                else: await sleepAsync(20)
-
-
-        elif req.url.path == "/hookMatchEndWS":
+        elif req.url.path == "/newUpdateWS":
             var ws = await newWebSocket(req)
-            var logsRead = 0
             while ws.readyState == Open:
-                if matchesLog.hasKey("matches"):
-                    if logsRead > matchesLog["matches"].len: logsRead = 0
-                    let numLogsToRead = matchesLog["matches"].len - 1
-                    if numLogsToRead > logsRead:
-                        var node = newJObject()
-                        var msg = newJArray()
-                        for i in logsRead..<numLogsToRead:
-                            msg.add(matchesLog["matches"][i])
-                        node.add("update", msg)
-                        await ws.send($node)
-                        logsRead = numLogsToRead
-                    else: await sleepAsync(20)
-                else: await sleepAsync(20)
-
-
-        elif req.url.path == "/hookKillWS":
-            var ws = await newWebSocket(req)
-            var logsRead = 0
-            while ws.readyState == Open:
-                if logsRead > killsLog.len: logsRead = 0
-                let numLogsToRead = killsLog.len
-                if numLogsToRead > logsRead:
-                    var node = newJObject()
-                    var msg = newJArray()
-                    for i in logsRead..<numLogsToRead:
-                        msg.add(killsLog[i])
-                    node.add("update", msg)
-                    await ws.send($node)
-                    logsRead = numLogsToRead
-                else: await sleepAsync(20)
-
-        elif req.url.path == "/hookChatWS":
-            var ws = await newWebSocket(req)
-            var logsRead = 0
-            while ws.readyState == Open:
-                if logsRead > chatLog.len: logsRead = 0
-                let numLogsToRead = chatLog.len
-                if numLogsToRead > logsRead:
-                    var node = newJObject()
-                    var msg = newJArray()
-                    for i in logsRead..<numLogsToRead:
-                        msg.add(chatLog[i])
-                    node.add("update", msg)
-                    await ws.send($node)
-                    logsRead = numLogsToRead
-                else: await sleepAsync(20)
+                let dataRecv = await ws.receiveStrPacket()
+                echo ":: updateData ",dataRecv
+                logHistory["old"].add parseJson(dataRecv)
+                await futureUpdate.write(dataRecv)
 
         elif req.url.path == "/":
-            #dataToSend = $req.headers & "\n<br/>" & dataToSend
-            #dataToSend = dataToSend & matchesLog.pretty
-            #html(body(pre(dataToSend)))
             var headResp = headers.newHttpHeaders()
             headResp["location"] = "/index.html"
             await req.respond(Http307, "", headResp)
@@ -254,55 +233,7 @@ proc runHTTPServer() {.thread.} =
             else:
                 poll()
 
-    proc updateTF2Console {.async.} =
-        proc updateMatches() {.async.} =
-            while true:
-                let tmp = chanTF2Matches.tryRecv()
-                if tmp.dataAvailable:
-                    # Reset kills chat etc..
-                    killsLog = @[]
-                    chatLog = @[]
-                    matchesLog = tmp.msg
-                else: await sleepAsync(18)
-
-        proc updateKills() {.async.} =
-            while true:
-                let tmp = chanTF2Kills.tryRecv()
-                if tmp.dataAvailable:
-                    killsLog.add(tmp.msg)
-                else: await sleepAsync(20)
-
-        proc updateChat() {.async.} =
-            while true:
-                let tmp = chanTF2Chat.tryRecv()
-                if tmp.dataAvailable:
-                    chatLog.add(tmp.msg)
-                else: await sleepAsync(20)
-
-        proc updateRawLog() {.async.} =
-            while true:
-                let lineLog = chan.tryRecv()
-                if lineLog.dataAvailable:
-                    lineRawLog.add($lineLog.msg) #.pretty
-                else: await sleepAsync(20)
-
-
-        asyncCheck updateMatches()
-        asyncCheck updateKills()
-        asyncCheck updateChat()
-        asyncCheck updateRawLog()
-
-    var activity : seq[Future[void]]
-
-    while true:
-        try:
-            activity.add(listenerHTTP())
-            activity.add(updateTF2Console())
-            runForever()
-        except:
-            discard
-            echo repr(getCurrentException())
-            echo "Error??????"
+    waitFor listenerHTTP()
 
 
 proc startTF2Logger*(filePath: string) =
@@ -311,25 +242,22 @@ proc startTF2Logger*(filePath: string) =
     logger.log(lvlDebug, ":: Starting Treads ::")
     FilePath = filePath
 
-    chan.open()
-    chanTF2Matches.open()
-    chanTF2Kills.open()
-    chanTF2Chat.open()
-
     createThread(thr[1], runHTTPServer)
 
     when isMainModule:
-        logger.log(lvlDebug, ":: Server starting wait 10 seconds ::")
-        sleep(10*1000) # wait 10 sec
-        createThread(thr[0], testWS)
+        logger.log(lvlDebug, ":: Server starting wait 8 seconds ::")
+        sleep(8*1000) # wait 8 sec
+        createThread(thr[0], tf2logger)
     else:
         createThread(thr[0], tf2logger)
 
     when(appType == "gui"):
+        import std/browsers
         openDefaultBrowser("http://127.0.0.1:" & $WebPort)
-    joinThreads thr
-    chan.close()
+
+    joinThreads(thr)
+    #chan.close()
 
 
 when isMainModule:
-    startTF2Logger()
+    startTF2Logger("")
